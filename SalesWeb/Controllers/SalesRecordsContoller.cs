7e6271de24c3b715d70e73f3039914a5967aff0d
Pedro Pontes <pedrinhopontes@hotmail.com>
2024-03-30 04:34:23 -0300

    Preparing sales search navigation views

---
+++ SalesWeb/Controllers/SalesRecordsContoller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWeb.Controllers
+{
+    public class SalesRecordsContoller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
