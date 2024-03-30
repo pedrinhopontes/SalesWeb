3540afe53a5c4a8f8c2ae216a1a76b662f0a4904
Pedro Pontes <pedrinhopontes@hotmail.com>
2024-03-30 01:51:41 -0300

    Update seller and custom service exception

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWeb.Models;
+using SalesWeb.Services.Exceptions;
 
 namespace SalesWeb.Services
 {
@@ -29,6 +30,24 @@ namespace SalesWeb.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update (Seller obj) 
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+            
         }
 
     }
