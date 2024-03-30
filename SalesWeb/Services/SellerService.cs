74500551f3e85d07f9c11893a5bca6d9a38cb73f
Pedro Pontes <pedrinhopontes@hotmail.com>
2024-03-30 01:17:45 -0300

    Seller details and eager loading

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWeb.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWeb.Models;
 
 namespace SalesWeb.Services
 {
@@ -21,7 +22,7 @@ namespace SalesWeb.Services
         }
         public Seller FindById(int id) 
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
